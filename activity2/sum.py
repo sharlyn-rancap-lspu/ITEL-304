73768d361380bd97e423eb7f7490192a0858e8f2
sharlyn-rancap-lspu <101610589+sharlyn-rancap-lspu@users.noreply.github.com>
2022-09-01 14:50:42 +0800

    Create sum.py

---
+++ activity2/sum.py
@@ -0,0 +1,12 @@
+
+#Python program  to add two numbers.
+
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+z = add(x, y)
+print(x, "+", y, "=", z)
