53b036814ab662f53a64cc68ec832eb798dd9c55
sharlyn-rancap-lspu <101610589+sharlyn-rancap-lspu@users.noreply.github.com>
2022-09-01 14:51:47 +0800

    Create area.py

---
+++ activity2/area.py
@@ -0,0 +1,9 @@
+#Python program  to find the area  of a triangle.
+
+def areaOfTriangle():
+    x = float(input("Enter the base of the triangle: "))
+    y = float(input("Enter the height of the triangle: "))
+    area = 1/2 * x * y
+    print("The area of the triangle  is: ", area)
+
+areaOfTriangle()
