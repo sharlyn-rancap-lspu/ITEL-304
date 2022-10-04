8b18cd3d62ae5d5cf1b5cc1d8e9dfdf23c34fcec
sharlyn-rancap-lspu <101610589+sharlyn-rancap-lspu@users.noreply.github.com>
2022-10-04 13:09:54 +0800

    Create app.py

---
+++ activity4/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return render_template('base.html')
+
+app.run(host="localhost", debug=True)
